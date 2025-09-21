eea2741ef7414b51ce28356edc8e50964dbf49be
MichaeLRoman <romanbanenko1@gmail.com>
2025-09-21 04:22:48 +0300

    a little

--- portfolio_bot/config.py
+++ portfolio_bot/config.py
@@ -4,7 +4,8 @@
 
 # --- Основные настройки ---
 # Токен вашего бота от @BotFather
-BOT_TOKEN = "7791557745:AAFXRRMJBx7dozu4cuez0tNHJDIW0EVTAaY"
+BOT_TOKEN = "8353031245:AAEAoHDIXjEGrf5DnrMKCuOaJftr9Q5Oj-U"
+# 8353031245:AAEAoHDIXjEGrf5DnrMKCuOaJftr9Q5Oj-U
 
 # ID администраторов бота (через запятую в .env) 
 ADMIN_IDS_STR = "6534859645, 1265245994, 1556788691"
@@ -13,7 +14,7 @@ ADMIN_IDS = [int(admin_id.strip()) for admin_id in ADMIN_IDS_STR.split(',') if a
 # --- URL-адреса ---
 # URL вашего Mini App (обязательно HTTPS)
 # Этот адрес будет использоваться и для Mini App, и для API.
-WEB_APP_BASE_URL = "https://97760f25e409.ngrok-free.app" # <-- ЗАМЕНИТЕ НА ВАШ АДРЕС NGROK
+WEB_APP_BASE_URL = "https://portfolio-yes.ru" # <-- ЗАМЕНИТЕ НА ВАШ АДРЕС NGROK
 
 # URL для кнопки в Telegram будет корневым
 MINI_APP_URL = WEB_APP_BASE_URL
